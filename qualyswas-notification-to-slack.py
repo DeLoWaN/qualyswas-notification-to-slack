35b8ebf6aac34907bd9aa04a08c41bae60dac4e9
kalumkalac <delovan@gmail.com>
2017-02-06 19:08:17 +0100

    deuz

--- qualyswas-notification-to-slack.py
+++ qualyswas-notification-to-slack.py
@@ -2,90 +2,74 @@
 
 import re
 import sys
+import slackweb
+import argparse
 
+# Parse arguments
+parser = argparse.ArgumentParser(description='Forwards Qualys WAS notification emails to Slack channel with severity control')
+parser.add_argument('-U', '--slack-url', required=True, help='URL of Slack Incoming Webhook')
+parser.add_argument('-S', '--severity', help='Severity on which the alert should be sent', choices=['urgent','critical','serious','medium','minimal'], default='critical')
+args = parser.parse_args()
+
+#slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+slack = slackweb.Slack(url=args.slack_url)
+
+# Prepare regexes
 status = re.compile(r"Scan Status :([A-Z\s]+[a-z\s]+):([A-Z\s]+[a-z\s]+)Authentication Status:([A-Z\s]+[a-z\s]+)")
-vulnerabilities = re.compile(r"Summary of Crawling(?: \(\+\/\-\)|)\n\nLinks Crawled : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\n\n\nSummary of Vulnerabilities(?: \(\+\/\-\)|)\n\nSeverity 5 \"Urgent\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 4 \"Critical\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 3 \"Serious\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 2 \"Medium\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 1 \"Minimal\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)")
+vulnerabilities = re.compile(r"Summary of Crawling(?: \(\+\/\-\)|)\s+Links Crawled : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+\nSummary of Vulnerabilities(?: \(\+\/\-\)|)\s+Severity 5 \"Urgent\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 4 \"Critical\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 3 \"Serious\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 2 \"Medium\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 1 \"Minimal\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)")
 
-text = '''Launch Type : Scheduled
+# Defines severity
+if args.severity == 'urgent':
+	severity = 5
+if args.severity == 'critical':
+	severity = 4
+if args.severity == 'serious':
+	severity = 3
+if args.severity == 'medium':
+	severity = 2
+if args.severity == 'minimal':
+	severity = 1
 
-Scan Status : Finished: Scan stopped after encountering too many errors on
-server side
-Authentication Status: Successful
-
-Next Action : N/A
-
-
-------------------------------------------------------------
-------------------------------------------------------------
-------------------
-
-Scan Statistics
-================================================
-
-Summary of Crawling
-
-Links Crawled : 303
-
-
-Summary of Vulnerabilities
-
-Severity 5 "Urgent" : 0 (=)
-Severity 4 "Critical" : 0
-Severity 3 "Serious" : 0 (+1)
-Severity 2 "Medium" : 0 (-4)
-Severity 1 "Minimal" : 2
-
-Total : 2
-
-
-Summary of Sensitive Contents
-
-Severity 5 "Urgent" : 0
-Severity 4 "Critical" : 0
-Severity 3 "Serious" : 0
-Severity 2 "Medium" : 0
-Severity 1 "Minimal" : 0
-
-Total : 0
-
-
-Summary of Information Gathered
-
-Severity 5 "Urgent" : 0
-Severity 4 "Critical" : 0
-Severity 3 "Serious" : 1
-Severity 2 "Medium" : 2
-Severity 1 "Minimal" : 16
-
-Total : 19
-'''
-
+# Read email from stdin
 text = ''.join(sys.stdin.readlines())
 
+# Grep statuses
 obj = status.search(text)
-
 scanstatus = obj.group(1).replace('\n',' ').strip()
 scanstatusdetail = obj.group(2).replace('\n',' ').strip()
 authenticationstatus = obj.group(3).replace('\n',' ').strip()
 
+# Grep vulnerabilities
 obj = vulnerabilities.search(text)
-
 linkscrawled = obj.group(1)
-
 urgent = obj.group(2)
 critical = obj.group(3)
 serious = obj.group(4)
 medium = obj.group(5)
 minimal = obj.group(6)
 
-print('Scan Status : {} - {}\nAuthentication Status: {}\n\nLinks Crawled: {}\n\nSummary of Vulnerabilities :\n\nSeverity 5 "Urgent" : {}\nSeverity 4 "Critical" : {}\nSeverity 3 "Serious" : {}\nSeverity 2 "Medium" : {}\nSeverity 1 "Minimal" : {}'.format(
-	scanstatus,
-	scanstatusdetail,
-	authenticationstatus,
-	linkscrawled,
-	urgent,
-	critical,
-	serious,
-	medium,
-	minimal
-	))
+# Notify
+sendNotification = False
+if  (scanstatus != 'Finished' or (scanstatus == 'Finished' and scanstatusdetail != 'OK')) or \
+	(authenticationstatus != 'Successful') or \
+	(int(urgent) > 0 and severity <= 5) or \
+	(int(critical) > 0 and severity <= 4) or \
+	(int(serious) > 0 and severity <= 3) or \
+	(int(medium) > 0 and severity <= 2) or \
+	(int(minimal) > 0 and severity <= 1):
+	sendNotification = True
+	print('NOTIF')
+
+
+
+#slack.notify(text='Scan Status : {} - {}\nAuthentication Status: {}\n\nLinks Crawled: {}\n\nSummary of Vulnerabilities :\n\nSeverity 5 "Urgent" : {}\nSeverity 4 "Critical" : {}\nSeverity 3 "Serious" : {}\nSeverity 2 "Medium" : {}\nSeverity 1 "Minimal" : {}'.format(
+#	scanstatus,
+	#scanstatusdetail,
+	#authenticationstatus,
+	#linkscrawled,
+	#urgent,
+	#critical,
+	#serious,
+	#medium,
+	#minimal
+	#))
