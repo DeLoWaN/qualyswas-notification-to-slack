a017643f128adf93b1158c401cdf9db35606640c
kalumkalac <delovan@gmail.com>
2017-02-07 18:29:55 +0100

    fix issues with email parsing

--- qualyswas-notification-to-slack.py
+++ qualyswas-notification-to-slack.py
@@ -7,6 +7,8 @@ import sys
 import slackweb
 import argparse
 import json
+import email
+from html2text import html2text
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Forwards Qualys WAS notification emails to Slack channel with severity control. Take the email in stdin')
@@ -18,7 +20,7 @@ slack = slackweb.Slack(url=args.slack_url)
 
 # Prepare regexes
 status = re.compile(r"Scan Status :([A-Z\s]+[a-z\s]+):([A-Z\s]+[a-z\s]+)Authentication Status:([A-Z\s]+[a-z\s]+)")
-vulnerabilities = re.compile(r"Summary of Crawling(?: \(\+\/\-\)|)\s+Links Crawled : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+\nSummary of Vulnerabilities(?: \(\+\/\-\)|)\s+Severity 5 \"Urgent\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 4 \"Critical\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 3 \"Serious\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 2 \"Medium\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\nSeverity 1 \"Minimal\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)")
+vulnerabilities = re.compile(r"Summary of Crawling(?: \(\+\/\-\)|)\s+Links Crawled : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+\nSummary of Vulnerabilities(?: \(\+\/\-\)|)\s+Severity 5 \"Urgent\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+Severity 4 \"Critical\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+Severity 3 \"Serious\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+Severity 2 \"Medium\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)\s+Severity 1 \"Minimal\" : (\d+)(?: \((?:=|\+\d+|\-\d+)\)|)")
 
 # Defines severity
 if args.severity == 'urgent':
@@ -33,47 +35,53 @@ if args.severity == 'minimal':
     severity = 1
 
 # Read email from stdin
-text = ''.join(sys.stdin.readlines())
+msg = email.message_from_string(''.join(sys.stdin.readlines()))
 
-# Grep statuses
-obj = status.search(text)
-scanstatus = obj.group(1).replace('\n',' ').strip()
-scanstatusdetail = obj.group(2).replace('\n',' ').strip()
-authenticationstatus = obj.group(3).replace('\n',' ').strip()
+for part in msg.walk():
+    if part.get_content_maintype() == 'multipart':
+        continue
+    text = html2text(str(part.get_payload(decode=True),'utf-8'))
 
-# Grep vulnerabilities
-obj = vulnerabilities.search(text)
-linkscrawled = obj.group(1)
-urgent = obj.group(2)
-critical = obj.group(3)
-serious = obj.group(4)
-medium = obj.group(5)
-minimal = obj.group(6)
+    # Grep statuses
+    obj = status.search(text)
+    scanstatus = obj.group(1).replace('\n',' ').strip()
+    scanstatusdetail = obj.group(2).replace('\n',' ').strip()
+    authenticationstatus = obj.group(3).replace('\n',' ').strip()
+
+    # Grep vulnerabilities
+    obj = vulnerabilities.search(text)
+    linkscrawled = obj.group(1)
+    urgent = obj.group(2)
+    critical = obj.group(3)
+    serious = obj.group(4)
+    medium = obj.group(5)
+    minimal = obj.group(6)
 
 
-color = '#008000' #Green
+    color = '#008000' #Green
 
-# Notify
-if  (scanstatus != 'Finished' or (scanstatus == 'Finished' and (scanstatusdetail != 'OK' and scanstatusdetail != 'Ok'))) or \
-    (authenticationstatus != 'Successful') or \
-    (int(urgent) > 0 and severity <= 5) or \
-    (int(critical) > 0 and severity <= 4) or \
-    (int(serious) > 0 and severity <= 3) or \
-    (int(medium) > 0 and severity <= 2) or \
-    (int(minimal) > 0 and severity <= 1):
-    color = '#FF0000'
+    # Notify
+    if  (scanstatus != 'Finished' or (scanstatus == 'Finished' and (scanstatusdetail != 'OK' and scanstatusdetail != 'Ok'))) or \
+        (authenticationstatus != 'Successful') or \
+        (int(urgent) > 0 and severity <= 5) or \
+        (int(critical) > 0 and severity <= 4) or \
+        (int(serious) > 0 and severity <= 3) or \
+        (int(medium) > 0 and severity <= 2) or \
+        (int(minimal) > 0 and severity <= 1):
+        color = '#FF0000'
 
-text = 'Scan Status : {} - {}\nAuthentication Status: {}\n\nLinks Crawled: {}'.format(
-    scanstatus,
-    scanstatusdetail,
-    authenticationstatus,
-    linkscrawled
-    )
-attachments = [{"color":color,"title":"Qualys Vulnerabilities Scan Results","text":text,"fields":[
-                        {"value":"Severity 5 (Urgent) : {}".format(urgent)},
-                        {"value":"Severity 4 (Critical) : {}".format(critical)},
-                        {"value":"Severity 3 (Serious) : {}".format(serious)},
-                        {"value":"Severity 2 (Medium) : {}".format(medium)},
-                        {"value":"Severity 1 (Minimal) : {}".format(minimal)},
-                    ]}]
-slack.notify(attachments=attachments)
+    text = 'Scan Status : {} - {}\nAuthentication Status: {}\n\nLinks Crawled: {}'.format(
+        scanstatus,
+        scanstatusdetail,
+        authenticationstatus,
+        linkscrawled
+        )
+    attachments = [{"color":color,"title":"Qualys Vulnerabilities Scan Results","text":text,"fields":[
+                            {"value":"Severity 5 (Urgent) : {}".format(urgent)},
+                            {"value":"Severity 4 (Critical) : {}".format(critical)},
+                            {"value":"Severity 3 (Serious) : {}".format(serious)},
+                            {"value":"Severity 2 (Medium) : {}".format(medium)},
+                            {"value":"Severity 1 (Minimal) : {}".format(minimal)},
+                        ]}]
+    slack.notify(attachments=attachments)
+    break
